60ae79ceca6909268f1c27f29cc1ec643bfb18a7
Binary Craft <elliot.huang.signed@gmail.com>
2023-03-08 12:58:03 +0800

    upgrade to zig master

--- build.zig
+++ build.zig
@@ -3,105 +3,64 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) !void {
     // Add standard target options
     const target = b.standardTargetOptions(.{});
-    // Add standard release options
-    const mode = b.standardReleaseOptions();
-    // Add the library
-    const uv = b.addStaticLibrary("uv", "src/uv.zig");
-    uv.setBuildMode(mode);
-    uv.install();
+    // Add standard optimize options
+    const optimize = b.standardOptimizeOption(.{});
+    // Add the module
+    const uv_module = b.addModule(
+        "uv",
+        .{
+            .source_file = .{ .path = "src/uv.zig" },
+        },
+    );
     // Add the unit tests
-    const unit_tests_step = b.step("test", "Run the unit tests");
-    const unit_tests = b.addTest("src/uv.zig");
-    unit_tests.setBuildMode(mode);
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/uv.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    const unit_tests_step = b.step("test", "Run unit tests");
     unit_tests_step.dependOn(&unit_tests.step);
     unit_tests.test_evented_io = true;
-    // Define the library package
-    const uv_pkg = std.build.Pkg{
-        .name = "uv",
-        .source = .{ .path = "src/uv.zig" },
-        .dependencies = &.{},
-    };
+
     // Add examples
-    const cgi = b.addExecutable("cgi", "examples/cgi/cgi.zig");
-    const detach = b.addExecutable("detach", "examples/detach.zig");
-    const dns = b.addExecutable("dns", "examples/dns.zig");
-    const locks = b.addExecutable("locks", "examples/locks.zig");
-    const onchange = b.addExecutable("onchange", "examples/onchange.zig");
-    const proc_streams = b.addExecutable("proc_streams", "examples/proc_streams.zig");
-    const progress = b.addExecutable("progress", "examples/progress.zig");
-    const queue_work = b.addExecutable("queue_work", "examples/queue_work.zig");
-    const signal = b.addExecutable("signal", "examples/signal.zig");
-    const spawn = b.addExecutable("spawn", "examples/spawn.zig");
-    const thread_create = b.addExecutable("thread_create", "examples/thread_create.zig");
-    const timer = b.addExecutable("timer", "examples/timer.zig");
-    const uvcat = b.addExecutable("uvcat", "examples/uvcat.zig");
-    const uvstop = b.addExecutable("uvstop", "examples/uvstop.zig");
-    const uvtee = b.addExecutable("uvtee", "examples/uvtee.zig");
+    const examples = [_][]const u8{
+        //"examples/cgi/cgi.zig",
+        "examples/detach.zig",
+        "examples/dns.zig",
+        "examples/locks.zig",
+        "examples/onchange.zig",
+        "examples/proc_streams.zig",
+        "examples/progress.zig",
+        "examples/queue_work.zig",
+        "examples/signal.zig",
+        "examples/spawn.zig",
+        "examples/thread_create.zig",
+        "examples/timer.zig",
+        "examples/uvcat.zig",
+        "examples/uvstop.zig",
+        "examples/uvtee.zig",
+    };
+
     // For each example
-    inline for (.{
-        cgi,
-        detach,
-        dns,
-        locks,
-        onchange,
-        proc_streams,
-        progress,
-        queue_work,
-        signal,
-        spawn,
-        thread_create,
-        timer,
-        uvcat,
-        uvstop,
-        uvtee,
-    }) |step| {
-        // Make sure they can be built and installed
-        step.setTarget(target);
-        step.setBuildMode(mode);
-        step.install();
-        // Add the library package
-        step.addPackage(uv_pkg);
-        // Add a run step
-        if (step.install_step) |install_step| {
-            const run_step_name = try std.mem.concat(
-                b.allocator,
-                u8,
-                &.{ "Run the `", step.name, "` example" },
-            );
-            const run_step = b.step(step.name, run_step_name);
-            const run_cmd = step.run();
-            run_cmd.step.dependOn(&install_step.step);
-            if (b.args) |args| {
-                run_cmd.addArgs(args);
-            }
-            run_step.dependOn(&run_cmd.step);
+    inline for (examples) |example| {
+        const name = example["examples/".len .. example.len - 4];
+        const exe = b.addExecutable(.{
+            .name = name,
+            .root_source_file = .{ .path = example },
+            .target = target,
+            .optimize = optimize,
+        });
+        exe.addModule("uv", uv_module);
+
+        exe.linkSystemLibrary("uv");
+        exe.linkLibC();
+        exe.install();
+        const run_cmd = exe.run();
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
         }
-    }
-    // Add the dependencies
-    inline for (.{
-        cgi,
-        detach,
-        dns,
-        locks,
-        onchange,
-        proc_streams,
-        progress,
-        queue_work,
-        signal,
-        spawn,
-        thread_create,
-        timer,
-        unit_tests,
-        uv,
-        uvcat,
-        uvstop,
-        uvtee,
-    }) |step| {
-        // Link the libraries
-        step.linkLibC();
-        step.linkSystemLibrary("libuv");
-        // Use the `stage1` compiler because of
-        // https://github.com/ziglang/zig/issues/12325
-        step.use_stage1 = true;
+        const run_step = b.step("run", "Run" ++ name);
+        run_step.dependOn(&run_cmd.step);
     }
 }
